3e28557ded502df9913bbce71ef2c0bcf6c25e76
Belagir <belagir.tedab@gmail.com>
2025-07-26 21:35:07 +0200

    shader + application doc

--- src/3dful/3dful_application.c
+++ src/3dful/3dful_application.c
@@ -1,4 +1,13 @@
-
+/**
+ * @file 3dful_application.c
+ * @author Gabriel Bédat
+ * @brief Implementation of window and opengl context creation procedures.
+ * @version 0.1
+ * @date 2025-07-26
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
 #include <stdarg.h>
 
 #include <SDL2/SDL_image.h>
@@ -6,11 +15,11 @@
 #include "3dful.h"
 
 /**
- * @brief
+ * @brief Initialize a SDL window and an OpenGL context.
  *
- * @param name
- * @param width
- * @param height
+ * @param[in] name
+ * @param[in] width
+ * @param[in] height
  * @return struct application
  */
 struct application application_create(const char *name, u32 width, u32 height)
@@ -19,12 +28,15 @@ struct application application_create(const char *name, u32 width, u32 height)
     struct logger *log = logger_create(stderr, LOGGER_ON_DESTROY_DO_NOTHING);
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        logger_log(log, LOGGER_SEVERITY_ERRO, "Failed to initialise SDL:\n%s", SDL_GetError());
+        logger_log(log, LOGGER_SEVERITY_ERRO,
+                "Failed to initialise SDL:\n%s", SDL_GetError());
         return (struct application) { };
     }
 
-    if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF) != (IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF)) {
-        logger_log(log, LOGGER_SEVERITY_ERRO, "Failed to initialise SDL_image:\n%s", IMG_GetError());
+    if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF)
+            != (IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF)) {
+        logger_log(log, LOGGER_SEVERITY_ERRO,
+                "Failed to initialise SDL_image:\n%s", IMG_GetError());
         return (struct application) { };
     }
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
@@ -37,22 +49,26 @@ struct application application_create(const char *name, u32 width, u32 height)
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
 
     /* Create our window centered at 512x512 resolution */
-    target.sdl_window = SDL_CreateWindow(name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+    target.sdl_window = SDL_CreateWindow(name,
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     if (!target.sdl_window) {
-        logger_log(log, LOGGER_SEVERITY_CRIT, "Failed to spawn window:\n%s", SDL_GetError());
+        logger_log(log, LOGGER_SEVERITY_CRIT,
+                "Failed to spawn window:\n%s", SDL_GetError());
         return (struct application) { };
     }
 
     /* Create our opengl context and attach it to our window */
     target.ogl_context = SDL_GL_CreateContext(target.sdl_window);
     if (!target.sdl_window) {
-        logger_log(log, LOGGER_SEVERITY_CRIT, "Failed to spawn opengl context:\n%s", SDL_GetError());
+        logger_log(log, LOGGER_SEVERITY_CRIT,
+                "Failed to spawn opengl context:\n%s", SDL_GetError());
         application_destroy(&target);
         return (struct application) { };
     }
 
-    /* This makes our buffer swap syncronized with the monitor's vertical refresh */
+    /* This makes our buffer swap syncronized with the monitor's vertical
+    refresh */
     SDL_GL_SetSwapInterval(1);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
@@ -61,9 +77,9 @@ struct application application_create(const char *name, u32 width, u32 height)
 }
 
 /**
- * @brief
+ * @brief Destroys a window / OpenGL context.
  *
- * @param target
+ * @param[inout] target
  */
 void application_destroy(struct application *target)
 {
