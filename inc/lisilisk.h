774e721f730aa38eb87037373396205160c66b00
Belagir <belagir.tedab@gmail.com>
2025-07-28 17:59:03 +0200

    hashmap

--- inc/lisilisk.h
+++ inc/lisilisk.h
@@ -4,16 +4,25 @@
 
 #include <stdint.h>
 
+#define LISK_HANDLE_NONE ((lisk_handle_t) 0)
+
 typedef uint32_t lisk_handle_t;
 
 void lisk_init(void);
 void lisk_deinit(void);
 
-void lisk_resize(uint16_t width, uint16_t height);
-void lisk_rename(const char *window_name);
+void lisk_resize(
+        uint16_t width,
+        uint16_t height);
+void lisk_rename(
+        const char *window_name);
+
+void lisk_model(
+        const char *name,
+        const char *obj_file);
 
 lisk_handle_t lisk_model_instanciate(
-        const char *obj_file,
+        const char *model_name,
         float (*pos)[3],
         float scale);
 
